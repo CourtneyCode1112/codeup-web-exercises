1868c4ec66cf672db7c5c06c08b419b1ecf773d5
exitclearbackhelpCourtneyCode1112 <courtneynholley@gmail.com>
2020-08-18 10:15:53 -0500

    fixed break & continue loop problem

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,15 +1,16 @@
 var oddNumbertoFifty = function() {
-    var userNumber = prompt("Please pick a number between 1-50.")
-    var output = "";
-    for (var i = 1; i < 50; i++) {
+    var userNumber = prompt("Please pick an odd number between 1-50.")
+    for (var i = 1; i <= 50; i++) {
         if (isNaN(parseInt(userNumber)) || parseInt(userNumber) < 0 || parseInt(userNumber) > 50) {
             userNumber = prompt("Sorry that is not a number that fits the criteria. Please try again.");
         } else if (i % 2 === 0) {
             continue;
-        } if (i === parseInt(userNumber)) {
+        } else if (i == userNumber) {
             console.log("Yikes! Skipping number: " + userNumber);
-        };
-        output += console.log("Here in an odd number: " + i);
+        } else {
+            console.log("Here in an odd number: " + i);
+        }
+        if (i >= 51) break;
     }
 }
 oddNumbertoFifty();
