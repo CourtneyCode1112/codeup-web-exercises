4fabbe11eee362741e0e2ebb4cf9ecbf7b566fd5
exitclearbackhelpCourtneyCode1112 <courtneynholley@gmail.com>
2020-08-18 09:32:46 -0500

    finished continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,13 +1,15 @@
-var oddNumbertoFifty = function(input = prompt("Please enter a number between 1-50.")) {
+var oddNumbertoFifty = function() {
+    var userNumber = prompt("Please pick a number between 1-50.")
     var output = "";
     for (var i = 1; i < 50; i++) {
-        if (isNaN(parseInt(input))) {
-            input = prompt("Sorry that is not a number that fits the criteria. Please try again.");
+        if (isNaN(parseInt(userNumber)) || parseInt(userNumber) < 0 || parseInt(userNumber) > 50) {
+            userNumber = prompt("Sorry that is not a number that fits the criteria. Please try again.");
         } else if (i % 2 === 0) {
             continue;
-        }
+        } if (i === parseInt(userNumber)) {
+            console.log("Yikes! Skipping number: " + userNumber);
+        };
+        output += console.log("Here in an odd number: " + i);
     }
-    output += console.log("Here in an odd number: " + i);
-    if (i !== 0) output += '\n';
 }
 oddNumbertoFifty();
