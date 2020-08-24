9579531dc0462551c8efb6aa51d89cf936ac9d7d
exitclearbackhelpCourtneyCode1112 <courtneynholley@gmail.com>
2020-08-24 16:28:32 -0500

    finished circle/math exercise

--- js/circle.js
+++ js/circle.js
@@ -8,18 +8,22 @@
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+            var areaOfCircle = Math.PI * Math.pow(circle.radius, 2)
+            return areaOfCircle; // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
+            if (doRounding === true) {
+                circle.area = Math.round(this.getArea());
+                console.log("Area of a circle with radius: " + circle.radius + ", is: " + circle.area);
+            } else {
+                circle.area = this.getArea();
+                console.log("Area of a circle with radius: " + circle.radius + ", is: " + circle.area);
+            }
+        },
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
-        }
     };
 
     // log info about the circle
@@ -30,7 +34,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
