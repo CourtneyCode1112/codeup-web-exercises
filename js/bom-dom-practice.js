9ae724004e813830e25abbdbff08455966f93ed0
exitclearbackhelpCourtneyCode1112 <courtneynholley@gmail.com>
2020-08-25 13:59:59 -0500

    added more dom exercises

--- js/bom-dom-practice.js
+++ js/bom-dom-practice.js
@@ -221,3 +221,40 @@ function saveUsername() {
     hout.innerText = usernameInput.value;
     console.log(usernameInput.value);
 }
+
+(function() {
+    var mainHeading = document.getElementById("main-heading");
+    mainHeading.innerHTML = "<em>Hello Codeup!</em>";
+})();
+
+(function() {
+    var paragraph1 = document.getElementById("demo4");
+    paragraph1.innerHTML = "<b>Hello IO!</b>";
+
+    var paragraph2 = document.getElementById("demoText");
+    paragraph2.innerText = "<b>Hello IO!</b>";
+})();
+
+(function() {
+    // Get the search link anchor by id
+    var searchLink = document.getElementById("search-link");
+
+    // Does an element have an attribute?
+    console.log(searchLink.hasAttribute("href")); // Prints true
+    console.log(searchLink.hasAttribute("class")); // Prints false
+
+    // Get an attribute value
+    console.log(searchLink.getAttribute("href")); // Prints "http://www.yahoo.com"
+
+    // Add or modify an attribute
+    searchLink.setAttribute("class", "btn btn-default");
+    console.log(searchLink.hasAttribute("class"));
+    // Adds the attribute class and sets it to "btn btn-default"
+
+    searchLink.setAttribute("href", "http://google.com");
+    console.log(searchLink.getAttribute("href"));
+    // Changes the href attribute to "http://google.com"
+
+    // Remove an attribute
+    searchLink.removeAttribute("class"); // Remove the class attribute
+})();
