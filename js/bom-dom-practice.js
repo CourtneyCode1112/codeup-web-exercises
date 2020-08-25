71905c99837df4bcd1d1d371c455ea2212e8bf48
exitclearbackhelpCourtneyCode1112 <courtneynholley@gmail.com>
2020-08-25 12:32:07 -0500

    finished up dom exercises

--- js/bom-dom-practice.js
+++ js/bom-dom-practice.js
@@ -120,9 +120,104 @@
 // to cancel the timeout, you can call
 // clearTimeout(timeoutId);
 // prior to the delay expiring
-function reloadFromCache() {
-    location.reload(); //reload page, possible from cache
-}
-function reloadFromServer() {
-    location.reload(true); //reload from server
+// function reloadFromCache() {
+//     location.reload(); //reload page, possible from cache
+// }
+// function reloadFromServer() {
+//     location.reload(true); //reload from server
+// }
+
+//---------DOM-----------
+
+    "use strict";
+    (function() {
+    var btnToClick = document.getElementById('btnToClick');
+
+    console.log(btnToClick); // prints <button id="btnToClick">Click Me</button>
+})();
+
+//     (function() {
+//     var paragraph = document.getElementById('demo');
+//     paragraph.innerHTML = "Message Content";
+// })();
+//
+// (function(){
+//     var header = document.getElementById('demo2');
+//     header.innerHTML = "IO ROCKS!";
+// })()
+
+(function() {
+    // Get all elements with class 'even'
+    var evenElements = document.getElementsByClassName('even');
+
+    // Print the first element
+    console.log(evenElements[0]); // Prints li.even.list-one-item
+
+    // Print all elements
+    for (var i = 0; i < evenElements.length; i++) {
+        console.log(evenElements[i]);
+    }
+
+    // Prints:
+    // li.even.list-one-item
+    // li.even.list-two-item
+
+    var listItems = document.getElementsByTagName('li');
+
+    // Print the first list item
+    console.log(listItems[0]); // Prints li.odd.list-one-item
+
+    // Print all the list items
+    for (var i = 0; i < listItems.length; i++) {
+        console.log(listItems[i]);
+    }
+
+    // Prints
+    // li.odd.list-one-item
+    // li.even.list-one-item
+    // li.odd.list-two-item
+    // li.even.list-two-item
+})();
+
+(function() {
+    // Get all elements with class 'even'
+    var bElements = document.getElementsByClassName('b');
+
+    // Print all elements
+    var output = "";
+    for (var i = 0; i < bElements.length; i++) {
+        output += bElements[i].outerHTML;
+    }
+    let pOut = document.getElementById('output');
+    pOut.innerHTML = output;
+    console.log(output);
+})();
+
+(function() {
+    // Get all elements with class 'even'
+    var paragraphTags = document.getElementsByTagName('p');
+    // Print all elements
+    var output = "";
+    for (var i = 0; i < paragraphTags.length; i++) {
+        output += paragraphTags[i].outerHTML;
+    }
+    let pOut = document.getElementById('output2');
+    pOut.innerHTML = output;
+    console.log(output);
+})();
+
+let hout = document.getElementById("demo3");
+(function() {
+    // get the "username" input
+    var usernameInput = document.forms.login.username;
+
+    // log the value of the "username" input
+    hout.innerText = usernameInput.value;
+
+})();
+
+function saveUsername() {
+    var usernameInput = document.forms.login.username;
+    hout.innerText = usernameInput.value;
+    console.log(usernameInput.value);
 }
