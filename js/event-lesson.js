a2611407931aab054abb240f8424df8f95290c7f
exitclearbackhelpCourtneyCode1112 <courtneynholley@gmail.com>
2020-08-26 09:55:27 -0500

    added even remover mini-lab

--- js/event-lesson.js
+++ js/event-lesson.js
@@ -28,3 +28,37 @@ var listenToMe = function (event2) {
 }
 
 document.getElementById('me').addEventListener('click', listenToMe)
+
+// MINI LAB
+// Add a second button to you first lab.
+//     Write a second event listener to remove 'listenToMe' from the 'me'.
+//     Register the snd listener to be called when the 2nd button is clicked.
+//     Test you code.
+
+var remover = function (event3) {
+    document.getElementById('me').removeEventListener('click', listenToMe);
+}
+
+document.getElementById('meDone').addEventListener('click', remover)
+
+
+
+//remove listener
+
+var listener = function (event) {
+    alert('You clicked the button!');
+}
+
+// add event listener to btn1
+var btn2 = document.getElementById('btn2');
+btn2.addEventListener('click', listener);
+
+var remover = function (event) {
+    // remove event listener from btn2
+    btn2.removeEventListener('click', listener);
+    console.log('Event listener removed.');
+}
+
+// add event listener to btn2
+var btn3 = document.getElementById('btn3');
+btn3.addEventListener('click', remover);
