416bcc2b285276223abaa9e92bdfd4734d1aa9d4
coryholley <courtneynholley@gmail.com>
2020-09-15 10:02:46 -0500

    test

--- js/fizz_buzz.js
+++ js/fizz_buzz.js
@@ -1,11 +1,11 @@
-
-$("button").click(function () {
-    var userNum = prompt("Pick a number between 1-100")
-    if (userNum % 3 === 0 && userNum % 5 === 0) {
-        $("#results").html("Fizz Buzz" + " " + userNum)
-    } else if (userNum % 3 === 0) {
-        $("#results").html("Fizz" + " " + userNum)
-    } else if (userNum % 5 === 0) {
-        $("#results").html("Buzz" + " " + userNum)
-    }
-})
+//
+// $("button").click(function () {
+//     var userNum = prompt("Pick a number between 1-100")
+//     if (userNum % 3 === 0 && userNum % 5 === 0) {
+//         $("#results").html("Fizz Buzz" + " " + userNum)
+//     } else if (userNum % 3 === 0) {
+//         $("#results").html("Fizz" + " " + userNum)
+//     } else if (userNum % 5 === 0) {
+//         $("#results").html("Buzz" + " " + userNum)
+//     }
+// })
