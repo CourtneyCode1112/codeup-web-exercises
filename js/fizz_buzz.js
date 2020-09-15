440c28fb66d61de995552c775a4ebc8c51d72525
exitclearbackhelpCourtneyCode1112 <courtneynholley@gmail.com>
2020-09-15 09:58:06 -0500

    new fizz buzz w/jquery

--- js/fizz_buzz.js
+++ js/fizz_buzz.js
@@ -1,9 +1,11 @@
-for (var i = 0; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("Fizz Buzz" + " " + i)
-    } else if (i % 3 === 0) {
-        console.log("Fizz" + " " + i)
-    } else if (i % 5 === 0) {
-        console.log("Buzz" + " " + i)
+
+$("button").click(function () {
+    var userNum = prompt("Pick a number between 1-100")
+    if (userNum % 3 === 0 && userNum % 5 === 0) {
+        $("#results").html("Fizz Buzz" + " " + userNum)
+    } else if (userNum % 3 === 0) {
+        $("#results").html("Fizz" + " " + userNum)
+    } else if (userNum % 5 === 0) {
+        $("#results").html("Buzz" + " " + userNum)
     }
-}
+})
