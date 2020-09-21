d8dce5fc4538c95ca58d4824cdc1269a154ec451
coryholley <courtneynholley@gmail.com>
2020-09-21 10:22:34 -0500

    fetch and .then exercise

--- js/fizz_buzz.js
+++ js/fizz_buzz.js
@@ -21,17 +21,29 @@
 //     }
 // }
 
-const fizzBuzzGame = (num = 30) => {
-    for (let i = 0; i<=num; i++) {
-        if (i% 3 === 0 && i % 5 === 0)
-            console.log(`fizzbuzz`);
-        else if (i % 3 === 0)
-            console.log(`fizz`);
-        else if (i % 5 === 0)
-            console.log(`buzz`)
-        else
-            console.log(`${i}`)
-    }
-}
+// const fizzBuzzGame = (num = 30) => {
+//     for (let i = 0; i<=num; i++) {
+//         if (i% 3 === 0 && i % 5 === 0)
+//             console.log(`fizzbuzz`);
+//         else if (i % 3 === 0)
+//             console.log(`fizz`);
+//         else if (i % 5 === 0)
+//             console.log(`buzz`)
+//         else
+//             console.log(`${i}`)
+//     }
+// }
+//
+// fizzBuzzGame(80)
 
-fizzBuzzGame(80)
+fetch("https://api.github.com/users")
+    .then(response => response.json())
+    .then(result => {
+        result.forEach(user => console.log(user))
+    })
+    .catch(error => console.log(error))
+
+fetch("https://swapi.dev/api/people/1/")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
