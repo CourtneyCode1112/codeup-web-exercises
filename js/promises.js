abc2a216d76baf0d883bfc1e754457caf8bdfff1
coryholley <courtneynholley@gmail.com>
2020-09-21 16:34:41 -0500

    added a different way of doing problem one

--- js/promises.js
+++ js/promises.js
@@ -1,10 +1,23 @@
 
-function gitUserLastPush() {
+function gitUserLastCommit() {
     fetch('https://api.github.com/users/coryholley/events', {headers: {'Authorization': 'GITHUB_API_KEY'}})
         .then(result => result.json())
         .then(data => console.log(data[0].created_at))
 }
-gitUserLastPush()
+gitUserLastCommit()
+
+
+let getLastCommit = (username) => {
+    let url = `https://api.github.com/users/${username}/events/public`;
+    return fetch(url, {headers: {'Authorization': `token ${GITHUB_API_KEY}`}})
+        .then(response => response.json())
+        .then(events => events.filter(event => event.type === "PushEvent"))
+        .then(pushEvent => console.log(pushEvent[0].created_at))
+}
+
+getLastCommit("coryholley")
+
+
 
 function wait(num) {
     return new Promise((resolve, reject) => {
@@ -20,5 +33,5 @@ function wait(num) {
 
 console.log(wait()); // pending promise
 
-wait(1000).then((num) => console.log(num))
-wait(3000).then((num) => console.log(num))
+wait(1000).then((message) => console.log(message))
+wait(3000).then((message) => console.log(message))
